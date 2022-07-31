d890beb2597f849f3e60decb3be39b502cf86980
razurs <ginoalgarcia@gmail.com>
2022-07-30 21:46:58 -0500

    Fin

---
+++ src/04-numbers.ts
@@ -0,0 +1,18 @@
+(() => {
+  let productPrice: number = 100;
+  productPrice = 12;
+
+  console.log('productPrice', productPrice);
+
+  let customerAge: number = 20;
+  customerAge = customerAge + 1;
+  console.log('customerAge', customerAge);
+})();
+let binario = 0b101101;
+let hexa = 0xFF;
+console.log(binario);
+console.log(hexa);
+
+let myundefined: undefined;
+console.log(myundefined);
+
